ae3582e2bbd80d862c9e252ca73f3f657bfe5b72
dinashinta <dezeel0104@gmail.com>
2021-06-24 11:19:50 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Heading />
+    <kalkulator />
+  </div>
+</template>
+
+<script>
+import Heading from "./components/Heading.vue";
+import kalkulator from "./components/kalkulator.vue";
+export default {
+  name: "App",
+  components: {
+    Heading,
+    kalkulator,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1471ce;
+  margin-top: 30px;
+}
+</style>
+
+
---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Dina - kalkulator",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: #fff;
+}
+</style>
